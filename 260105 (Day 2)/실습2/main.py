c4d106f67ae6fdc2ee2382601e8f8f4f1d33f833
ruby-kim <dev.rubykim@gmail.com>
2026-01-05 14:42:00 +0900

    feat: update ex02

--- 260105 (Day 2)/실습2/main.py
+++ 260105 (Day 2)/실습2/main.py
@@ -5,7 +5,27 @@ from datetime import datetime
 app = FastAPI()
 
 class Reservation(BaseModel):
-    pass
+    name: str = Field(..., max_length=50, description="이름 길이는 최대 50자입니다")
+    email: str
+    date: datetime
+    special_requests: str = Field(default="", description="optional")
+
+    # date validation
+    @field_validator("date")
+    @classmethod
+    def validate_date(cls, value: datetime):
+        if value < datetime.now(): # 과거
+            raise ValueError("반드시 미래여야해요")
+        return value
+
+    # name validation
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value: str):
+        if len(value) > 50:
+            raise ValueError("50자 이내로 하세요")
+        return value
+
 
 @app.post("/reservations/")
 def create_reservation(reservation: Reservation):
